a2592e30196cdc3f3e5035e04c03a5efe55fef69
andrewgreen5610 <andrewgreen5610@gmail.com>
2015-02-04 03:10:24 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := trltetmo
+PRODUCT_RELEASE_NAME := trlteatt
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/trltetmo/device.mk)
+$(call inherit-product, device/samsung/trlteatt/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := trltetmo
-PRODUCT_NAME := cm_trltetmo
+PRODUCT_DEVICE := trlteatt
+PRODUCT_NAME := cm_trlteatt
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := trltetmo
+PRODUCT_MODEL := trlteatt
 PRODUCT_MANUFACTURER := samsung
