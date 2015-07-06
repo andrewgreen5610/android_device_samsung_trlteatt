b2ed9964715d926cf24df7a713021f06f9205a2e
andrewgreen5610 <andrewgreen5610@gmail.com>
2015-07-06 01:11:16 -0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/trlteatt/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := trlteatt
-PRODUCT_NAME := cm_trlteatt
+PRODUCT_NAME := trlteuc
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := trlteatt
+PRODUCT_MODEL := SAMSUNG-SM-N910A
 PRODUCT_MANUFACTURER := samsung
